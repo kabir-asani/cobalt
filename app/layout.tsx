67d7064bca1a49d01cfd168636cca7c462b506a7
Kabir Asani <iamkabirasani@gmail.com>
2025-04-15 10:40:20 +0530

    Update Open Graph image URL in metadata for improved sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
     description:
       "Explore the fascinating world of cobalt. From its role in clean energy to cutting-edge technology, Cobalt is your guide to this powerful metal.",
     images: {
-      url: "/og-card.png",
+      url: "https://cobalt-plum.vercel.app/og-card.png",
       width: 1200,
       height: 628,
     },
